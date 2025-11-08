e599edf148814a480231c9b3d7e9cd589e3df1d1
Leonard Pfeiffer <leonard.s.pfeiffer@gmx.net>
2025-11-08 16:48:50 +0100

    Reset Index on Command Execution

--- src/main/java/com/cavetale/setup/Setup.java
+++ src/main/java/com/cavetale/setup/Setup.java
@@ -2,7 +2,12 @@ package com.cavetale.setup;
 
 import com.cavetale.setup.console.CustomCommand;
 import com.cavetale.setup.console.CustomFlag;
+import com.cavetale.setup.data.plugin.Category;
+import com.cavetale.setup.data.plugin.Plugin;
+import com.cavetale.setup.data.plugin.Server;
+import com.cavetale.setup.data.server.Software;
 import io.github.ijustleyxo.jclix.app.App;
+import io.github.ijustleyxo.jclix.app.event.type.PreCmdEvent;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
@@ -14,7 +19,16 @@ public final class Setup {
     public static final @NotNull File TEMP = new File(".setup/");
 
     public static void main(String[] args) {
-        new App(CustomCommand.values(), CustomFlag.values(), args);
+        App app = new App(CustomCommand.values(), CustomFlag.values());
+        app.listeners().add(PreCmdEvent.class, Setup::reset);
+        app.run(args);
+    }
+
+    private static void reset(@NotNull PreCmdEvent e) {
+        Category.reset();
+        Plugin.reset();
+        Server.reset();
+        Software.reset();
     }
 
     // TODO: Resource pack download (with hash)
